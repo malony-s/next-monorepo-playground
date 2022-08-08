0368e8e5efb8be7f5da537a724be4e6a92a2c26b
beomsoo park <loki@villain-market.com>
2022-08-09 02:29:55 +0900

    @shared/ui uitls withDivider init
    @shared/ui
    - uitls/withDivider 컴포넌트 래퍼 추가

---
+++ shared/ui/src/utils/withDivider.tsx
@@ -0,0 +1,29 @@
+import React, { PropsWithChildren } from "react";
+
+export type WithDivider = { divider?: JSX.Element };
+
+const withDivider =
+  <T extends WithDivider>(Component: React.FC<T>) =>
+  ({ divider, children, ...others }: PropsWithChildren<T>) => {
+    const count = React.Children.count(children);
+    const Dividers = divider
+      ? new Array(count - 1).fill("").map(() => React.cloneElement(divider))
+      : [];
+    const childrenArray = React.Children.toArray(children);
+    Dividers.forEach((v, i) => {
+      childrenArray.splice(
+        i * 2 + 1,
+        0,
+        React.cloneElement(v, {
+          key: v.type + "divider" + i,
+        })
+      );
+    });
+
+    return React.createElement(Component, {
+      ...others,
+      children: <>{childrenArray}</>,
+    } as PropsWithChildren<T>);
+  };
+
+export default withDivider;
