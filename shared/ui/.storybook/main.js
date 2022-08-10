d58af3fe4a18da618394a0a4f0633a965484344d
beomsoo park <loki@villain-market.com>
2022-08-11 01:55:03 +0900

    update .storybook/main
    1. add tsconfig-path-webpack-plugin

--- shared/ui/.storybook/main.js
+++ shared/ui/.storybook/main.js
@@ -1,9 +1,15 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
 };
