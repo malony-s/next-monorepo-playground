3f0c1360fb241bf2ac7fb4a8a80387326abdf8ec
beomsoo park <loki@villain-market.com>
2022-08-11 01:53:55 +0900

    update root eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.export = {
   root: true,
-  extends: ['prettier'],
+  extends: ['prettier', '@emotion'],
   parserOptions: {
     project: ['./tsconfig.base.json'],
+  },
+  rules: {
+    '@emotion/jsx-import': 'error',
+    '@emotion/no-vanilla': 'error',
+    '@emotion/import-from-emotion': 'error',
+    '@emotion/styled-import': 'error',
   },
 };
